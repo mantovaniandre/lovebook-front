6a67f97f7e46c451b21a84f372253d23989209b9
mantovaniandre <mantovaniandre@outlook.com.br>
2021-11-23 23:17:07 -0300

    Atualização

--- lovebook/src/app/account-employee/account-employee.component.ts
+++ lovebook/src/app/account-employee/account-employee.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Livro } from '../models/Livro';
+import { ConnectionApiService } from '../services/connection-api.service';
 
 @Component({
   selector: 'app-account-employee',
@@ -13,9 +17,34 @@ export class AccountEmployeeComponent implements OnInit {
     console.log(this.group.value);
 }
 
-  constructor() { }
+data!: Array<any>;
+$livros!: Observable<Array<Livro>>;
 
+constructor(private service: ConnectionApiService,
+  private router: Router) {
+  this.data = new Array<any>();
+}
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
+slides: any = [[]];
+  chunk(arr: string | any[], chunkSize: number) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
+
+getLivros(livro: string){
+  event?.preventDefault();
+  this.$livros = this.service.getBooks(livro, "");
+  this.$livros.subscribe(data => {
+    console.log(data);
+    this.data = data;
+    this.slides = this.chunk(this.data, 3);
+  })
+}
 
 }
