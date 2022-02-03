37b02c2e6471d41a491f33b755ca50ec54658379
mantovaniandre <andreluizdiasmantovani@gmail.com>
2022-02-03 12:05:13 -0300

    Atualização

--- lovebook/src/app/services/connection-api.service.ts
+++ lovebook/src/app/services/connection-api.service.ts
@@ -12,7 +12,7 @@ export class ConnectionApiService {
   constructor(private http: HttpClient) { }
 
   getBooks(Livro: string, Categoria: string, Autor: string, Editora: string): Observable<any> {
-    let url = 'http://localhost:8080/book'
+    let url = 'http://localhost:8081/book'
     const params = new HttpParams()
       .set('nome', Livro)
       .set('categoria', Categoria)
@@ -23,91 +23,91 @@ export class ConnectionApiService {
   }
 
   identificacaoLivro(): Observable<any>{
-    let url = 'http://localhost:8080/book'
+    let url = 'http://localhost:8081/book'
     return this.http.get(url)
   }
 
   login(credentials: any): Observable<any>{
-    let url = 'http://localhost:8080/auth'
+    let url = 'http://localhost:8081/auth'
     return this.http.post(url, credentials)
   }
 
   identificacaoUsuario(): Observable<any>{
-    let url = 'http://localhost:8080/user'
+    let url = 'http://localhost:8081/user'
     return this.http.get(url)
   }
 
   postRegistrar(registrar: any): Observable<any>{
-    let url = 'http://localhost:8080/user'
+    let url = 'http://localhost:8081/user'
     return this.http.post(url, registrar);
   }
 
   putUsuario(atualizarCadastro: any): Observable<any>{
-    let url = 'http://localhost:8080/user'
+    let url = 'http://localhost:8081/user'
     return this.http.put(url, atualizarCadastro);
   }
 
   getComentarios(Id: any): Observable<any>{
-    let url = 'http://localhost:8080/comments'
+    let url = 'http://localhost:8081/comments'
     const params = new HttpParams()
       .set('idDoLivro', Id)
     return this.http.get(url, {params})
   }
 
   postComentarios(comentario: any): Observable<any>{
-    let url = 'http://localhost:8080/comments'
+    let url = 'http://localhost:8081/comments'
     return this.http.post(url, comentario);
   }
 
   postPurchase(listaLivros: any): Observable<any>{
-    let url = 'http://localhost:8080/purchase'
+    let url = 'http://localhost:8081/purchase'
     return this.http.post(url, listaLivros);
   }
 
   pegarComentarios(): Observable<any>{
-    let url = 'http://localhost:8080/comments/user'
+    let url = 'http://localhost:8081/comments/user'
     return this.http.get(url);
   }
 
   deleteComentarios(idComentario: any): Observable<any>{
-    let url = 'http://localhost:8080/comments/'+idComentario
+    let url = 'http://localhost:8081/comments/'+idComentario
     return this.http.delete(url);
   }
 
   getPurchase(): Observable<any> {
-    let url = 'http://localhost:8080/purchase'
+    let url = 'http://localhost:8081/purchase'
     return this.http.get(url);
   }
 
   deleteBook(id: any): Observable<any> {
-    let url = 'http://localhost:8080/book'
+    let url = 'http://localhost:8081/book'
     const params = new HttpParams()
       .set('id', id)
     return this.http.delete(url, {params});
   }
 
   putBook(atualizarLivro: any): Observable<any>{
-    let url = 'http://localhost:8080/book'
+    let url = 'http://localhost:8081/book'
     return this.http.put(url, atualizarLivro);
   }
 
   postBook(livro: any): Observable<any>{
-    let url = 'http://localhost:8080/book'
+    let url = 'http://localhost:8081/book'
     return this.http.post(url, livro)
   }
 
   deleteComentario(): Observable<any>{
-    let url = 'http://localhost:8080/comments'
+    let url = 'http://localhost:8081/comments'
     return this.http.delete(url);
   }
 
   getGlobalReport(): Observable<any>{
-    let url = 'http://localhost:8080/dashboard/global'
+    let url = 'http://localhost:8081/dashboard/global'
     return this.http.get(url);
   }
 
   getStatsReport(mes: number, ano: number): Observable<any>{
-    let url = 'http://localhost:8080/dashboard/stats'
+    let url = 'http://localhost:8081/dashboard/stats'
     const params = new HttpParams()
     .set('mes', mes)
     .set('ano', ano)
